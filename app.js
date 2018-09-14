b3818edffe7c981c570ad29bce6618e536ef3c54
ChrisWaino91 <34106482+ChrisWaino91@users.noreply.github.com>
2018-09-14 17:04:41 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,68 @@
+//Define UI Vars
+
+const tempInput = document.querySelector('#temperature');
+const tempType = document.querySelector('#type');
+const form = document.querySelector('#convert-form');
+let message = document.querySelector('.result');
+let result; 
+
+
+// Load all event listeners
+loadEventListeners();
+
+
+// Load event listeners
+function loadEventListeners(){
+    // Get the user's inputted temperature
+    form.addEventListener('submit', userTemperature);
+    // Get the user's inputted conversion type
+    form.addEventListener('change', userConversionType);
+    // Calculate the conversion
+    form.addEventListener('submit', calculateTemp);
+}
+
+
+// Get Inputted Temperature Function
+function userTemperature(e){
+    e.preventDefault();
+}
+
+
+// Get Selected Conversion Type
+function userConversionType(e){
+    e.preventDefault();
+}
+
+
+// Calculate the Temperature Conversion
+function calculateTemp(e){
+    if(tempInput.value === ''){
+        errorMessage();
+    }
+    else if(tempType.value === "1"){
+        console.log('Calculating Celcius To Fahrenheit...');
+        result = (tempInput.value * 1.8) + 32;
+        result = result.toFixed(2);
+        console.log(`${tempInput.value} is equal to ` + result + ` Degrees Fahrenheit`);
+        setMessage();
+    } else {
+        console.log('Calculating Fahrenheit To Celcius...');
+        result = (tempInput.value - 32) * 5 / 9;
+        result = result.toFixed(2);
+        console.log(`${tempInput.value} is equal to ` + result + ` Degrees Celcius`);
+        setMessage();
+    }
+}
+
+
+// Set Message - This makes it a bit cleaner rather than putting it all in the same calculateTemp function...
+function setMessage(){
+    message.textContent = `${tempInput.value} is equal to ` + result + ` Degrees Fahrenheit`;
+    message.style.color = 'initial';
+}
+
+// Error Message - Setting the error message if nothing has been input
+function errorMessage(){
+    message.textContent = `Please ensure that you have entered a value above!`;
+    message.style.color = 'red';
+}
